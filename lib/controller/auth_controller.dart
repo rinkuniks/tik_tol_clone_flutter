ff310fbdf0b842ebc2dbda109e631747c17eae54
Nikhil Kumar Singh <rinkuniks@gmail.com>
2022-11-29 15:59:58 +0530

    auth controller with Firebase and storage Setup

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,5 +1,44 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 
-class AuthController extends GetxController{
+class AuthController extends GetxController {
+  //User register
+  void signUp(
+    String username,
+    String email,
+    String password,
+    File? image,
+  ) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        UserCredential credential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password);
+        _uploadProPic(image);
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+        Get.snackbar("Error Occurred", e.toString());
+      }
+    }
+  }
 
+  Future<String> _uploadProPic(File img) async {
+    Reference ref = FirebaseStorage.instance
+        .ref()
+        .child('profilePics')
+        .child(FirebaseAuth.instance.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putFile(img);
+    TaskSnapshot snapshot = await uploadTask;
+    String imageDwnUrl = await snapshot.ref.getDownloadURL();
+    return imageDwnUrl;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tik_tol_clone_flutter/controller/auth_controller.dart';
 import 'package:tik_tol_clone_flutter/view/screens/auth/signup_screen.dart';
 import 'constant.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp().then((value){
+    Get.put(AuthController());
+  });
   runApp(const MyApp());
 }
 
@@ -26,4 +29,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
